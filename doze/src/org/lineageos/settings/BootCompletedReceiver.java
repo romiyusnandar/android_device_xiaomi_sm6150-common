885c6a86f55aee862a73def61c3ca3d269d8804d
romiyusnandar <yusromi04@gmail.com>
2025-03-12 02:15:05 +0000

    sm6150-common: Drop thermal settings
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- doze/src/org/lineageos/settings/BootCompletedReceiver.java
+++ doze/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,11 +20,9 @@ package org.lineageos.settings.doze;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -32,13 +30,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        Log.d(TAG, "Received intent: " + intent.getAction());
-        if (!intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
-            return;
-        }
-
-        Log.i(TAG, "Boot completed, starting services");
-	DozeUtils.onBootCompleted(context);
-        ThermalUtils.startService(context);
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+        DozeUtils.onBootCompleted(context);
     }
 }
